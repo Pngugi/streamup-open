647d901c235eda03c3872406789b98d40710b53b
richard457 <beastar457@gmail.com>
2017-01-10 13:39:14 +0200

    ...

---
+++ app_modules/filesAndFolderChanges.js
@@ -0,0 +1,16 @@
+// function watchFileAndFolderChanges(){    
+//             try {
+//                   chokidar.watch(osAppPath, {ignored: /[\/\\]\./}).on('all', function(event, path) {
+//                 if(event === "unlink"){
+                  
+//                 }else if(event === "add"){
+//                     console.log("folder created..:"+path);
+//                 }
+                
+//             });
+//             } catch (error) {
+                  
+//           }
+            
+//     };
+// watchFileAndFolderChanges();
