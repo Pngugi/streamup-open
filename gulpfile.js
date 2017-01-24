06dc010d30badb72d91f794ee51991ba6a3e9cf1
richard457 <beastar457@gmail.com>
2017-01-24 14:22:40 +0200

    working...

--- gulpfile.js
+++ gulpfile.js
@@ -20,4 +20,36 @@ gulp.task('watch', function() {
 		.pipe(gulp.dest(''));
 });
 
+
+
+
+//distribution tasks
+var run = require('gulp-run'),
+	asar = require('asar'),
+	clean = require('gulp-clean')
+;
+gulp.task('clean',function(){
+	return gulp.src('package',{read:false})
+	.pipe(clean({fodler:true}));
+});
+gulp.task('copy-app',['clean'],function(){
+	return gulp.src(['src/**/*','Views/**/**','package.json'],{base: '.'})
+	.pipe(gulp.dest('package'));
+});
+gulp.task('package',['copy-app'],function(){
+	return gulp.src('package/**/*')
+	.pipe(asar('app.asar'))
+	.pipe(gulp.dest('build'));
+});
+
+gulp.task('run',function(){
+	return run('electon . --all').exec();
+});
+
+
+//load linux compile structure
+require('require-dir')('./build/linux');
+gulp.task(['buildRpmPackage'], function () {
+  
+});
 gulp.task('default', ['compile']);
