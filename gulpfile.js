aba32b51fb8fa401e246ca7c36095e5aa3a902a9
richard457 <beastar457@gmail.com>
2017-02-05 08:42:06 +0200

    ..

--- gulpfile.js
+++ gulpfile.js
@@ -1,55 +1,33 @@
 var gulp = require('gulp');
-var tsb = require('gulp-tsb');
-var util = require('./build/lib/util');
-var watcher = require('./build/lib/watch');
-var assign = require('object-assign');
+var deb = require('gulp-deb');
+var fs = require("fs");
 
-var compilation = tsb.create(assign({ verbose: true }, require('./tsconfig.json').compilerOptions));
+gulp.task('default', function () {
 
-gulp.task('compile', function() {
-	return gulp.src('src/**/*.ts', { base: '.' })
-		.pipe(compilation())
-		.pipe(gulp.dest(''));
+//   var preinstContent = fs.readFileSync("scripts/preinst", "utf8");
+//   var postrmContent = fs.readFileSync("scripts/postrm", "utf8");
+
+  return gulp
+    .src([
+      'src/**',
+      'node_modules/**',
+      '!**/.git/**'
+    ], { base: process.cwd() })
+    .pipe(deb('Sbox.deb', {
+      name: 'example',
+      version: '1.0.0-1',
+      maintainer: {
+        name: 'Muragijimana Richard',
+        email: 'beastar457@gmail.com'
+      },
+      short_description: 'Open Ap',
+      long_description: ''
+	//   ,
+    //   //...
+    //   scripts: {
+    //     preinst: preinstContent,
+    //     postrm: postrmContent
+    //   }
+    }))
+    .pipe(gulp.dest('build/'));
 });
-
-gulp.task('watch', function() {
-	var src = gulp.src('**/*.ts', { base: '.' });
-
-	return watcher('**/*.ts', { base: '.' })
-		.pipe(util.incremental(compilation, src))
-		.pipe(gulp.dest(''));
-});
-
-
-
-
-//distribution tasks
-var run = require('gulp-run'),
-	asar = require('asar'),
-	clean = require('gulp-clean')
-;
-gulp.task('clean',function(){
-	return gulp.src('package',{read:false})
-	.pipe(clean({fodler:true}));
-});
-gulp.task('copy-app',['clean'],function(){
-	return gulp.src(['src/**/*','Views/**/**','package.json'],{base: '.'})
-	.pipe(gulp.dest('package'));
-});
-gulp.task('package',['copy-app'],function(){
-	return gulp.src('package/**/*')
-	.pipe(asar('app.asar'))
-	.pipe(gulp.dest('build'));
-});
-
-gulp.task('run',function(){
-	return run('electon . --all').exec();
-});
-
-
-//load linux compile structure
-require('require-dir')('./build/linux');
-gulp.task(['buildRpmPackage'], function () {
-  
-});
-gulp.task('default', ['compile']);
